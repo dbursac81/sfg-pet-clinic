3179ff6c615592df0e7925eb32ee5ef1f48a5369
davorbursac <davor.bursac@ag04.com>
2021-05-28 12:23:39 +0200

    create speciality map service impl. closes #20

---
+++ sfg-pet-clinic/pet-clinic-data/src/main/java/guru/springframework/services/SpecialtiesService.java
@@ -0,0 +1,9 @@
+package guru.springframework.services;
+
+import guru.springframework.model.Speciality;
+import org.springframework.stereotype.Service;
+
+@Service
+public interface SpecialtiesService extends CrudService<Speciality, Long>{
+
+}
