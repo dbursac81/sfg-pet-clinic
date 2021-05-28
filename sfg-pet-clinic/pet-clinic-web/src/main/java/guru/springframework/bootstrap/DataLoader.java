c8c9e8df00feec7b93f79df1adf3d95c2b157476
davorbursac <davor.bursac@ag04.com>
2021-05-28 11:18:13 +0200

    add PetTypes to bootstrap folder (startup). closes #19

--- sfg-pet-clinic/pet-clinic-web/src/main/java/guru/springframework/bootstrap/DataLoader.java
+++ sfg-pet-clinic/pet-clinic-web/src/main/java/guru/springframework/bootstrap/DataLoader.java
@@ -1,8 +1,10 @@
 package guru.springframework.bootstrap;
 
 import guru.springframework.model.Owner;
+import guru.springframework.model.PetType;
 import guru.springframework.model.Vet;
 import guru.springframework.services.OwnerService;
+import guru.springframework.services.PetTypeService;
 import guru.springframework.services.VetService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
@@ -12,15 +14,26 @@ public class DataLoader implements CommandLineRunner{
 
     private final OwnerService ownerService;
     private final VetService vetService;
+    private final PetTypeService petTypeService;
 
 
-    public DataLoader(OwnerService ownerService, VetService vetService) {
+    public DataLoader(OwnerService ownerService, VetService vetService, PetTypeService petService) {
         this.ownerService = ownerService;
         this.vetService = vetService;
+        this.petTypeService = petService;
     }
 
     @Override
     public void run(String... args) throws Exception {
+
+        PetType dog = new PetType();
+        dog.setName("Dog");
+        PetType savedDogPetType = petTypeService.save(dog);
+
+        PetType cat = new PetType();
+        dog.setName("Cat");
+        PetType savedCatPetType = petTypeService.save(cat);
+
         Owner owner1 = new Owner();
         owner1.setFirstName("Michael");
         owner1.setLastName("Weston");
